9d3f9971e38f888b5bc232ab1be2772273d79846
Danilo Zutin <dgzutin@gmail.com>
2015-02-14 16:44:31 +0100

    Call script to start experiment

---
+++ proxy.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: garbi
+ * Date: 2/14/15
+ * Time: 4:05 PM
+ */
+ini_set('display_errors', 0);
+
+$lab = $_GET["lab"];
+
+file_get_contents("http://www.rexlab.net/experimentos/conversao/mswConversao.php");
